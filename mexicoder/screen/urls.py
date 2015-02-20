b000273d090a201a89f0f0dd4b57a829151df3e4
m <m@arkham.hell>
2015-02-20 04:28:46 -0600

    Committer: José Carlos Tzompantzi de Jesús <radix_10339@hotmail.com> 	modified:   mexicoder/screen/urls.py

--- mexicoder/screen/urls.py
+++ mexicoder/screen/urls.py
@@ -9,6 +9,6 @@ router.register(r'coaches', TeamCoachViewSet)
 router.register(r'players', PlayerViewSet)
 
 urlpatterns = [
-    url(r'^$', include(router.urls)),
-    url(r'^api-auth/$', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
